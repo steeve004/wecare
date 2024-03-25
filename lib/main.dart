bcdf94daa813a6c583ce573abe0fb75f612c7343
Hismahjesty <tmyniwa@gmail.com>
2024-03-25 17:39:33 -0400

    identifier bundle  for Podfile

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:wecare/screens/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
