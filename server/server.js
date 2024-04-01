6b56c993d65e6ee77d0581be1b6a19e2a666f68c
Adewale Adegbesan <adegbesansteeve@gmail.com>
2024-04-01 18:01:18 -0400

    signin server2

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+//const bcrypt = require('bcrypt');
 
 
 // Initialize Express app
